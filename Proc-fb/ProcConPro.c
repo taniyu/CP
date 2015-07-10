1cdf55f2a3705735ebb4a7778aa5a2a0db8862e6
Yuji TANIMOTO <y.tanimoto13@gmail.com>
2015-07-11 02:45:30 +0900

    refs #2 入力プロセス出力プロセス固定での実装

--- Proc-fb/ProcConPro.c
+++ Proc-fb/ProcConPro.c
@@ -7,9 +7,21 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/sem.h>
+#include <time.h>
 
 #define	STEP   100000 // 10万回のインクリメント毎に表示 
 #define	TOTAL 1000000 // 全体で100万回インクリメントする 
+#define MAX_PID 100  // プロセスの最大数
+#define QSIZE  100  // メッセージキュー領域の大きさ
+
+// キューの宣言
+typedef struct {
+  int wptr;  // 入力位置
+  int rptr;   // 出力位置
+  int buff[QSIZE]; // データ格納用配列
+  int size;   // データ数
+} _queue;
+
 
 void P(int semid)
 {
@@ -31,27 +43,80 @@ void V(int semid)
   semop(semid, &sops, 1);	// Omitt error handling
 }
 
+// データをキューに書き込む 入力側の処理
+void write_data(int l, int semid, _queue *x)
+{
+  int count = 0;
+  int data, sleep_time;
+  while ( 1 ) {
+    if ( count > l ) { return; }
+    data = rand() / (RAND_MAX + 1.0) * 61 + 20;
+    P(semid);
+    if ( x->size < QSIZE ) {
+      x->buff[x->wptr++] = data;
+      x->wptr %= l;
+      x->size++;
+      count++;
+    }
+    V(semid);
+    printf("write size: %d %d", x->size, data);
+    sleep_time = rand() / (RAND_MAX + 1.0) * 61 + 20;
+    printf(", %d\n", sleep_time);
+    usleep(sleep_time);
+  }
+}
+
+// データをキューから読み出す 出力側の処理
+void read_data(int m, int semid, _queue *x)
+{
+  int count = 0;
+  int data;
+  while ( 1 ) {
+    if ( count > m ) { return; }
+    P(semid);
+    if ( x->size > 0 ) {
+      data = x->buff[x->rptr++];
+      x->rptr %= m;
+      x->size--;
+      count++;
+    }
+    V(semid);
+    printf("read size: %d", x->size);
+    printf(", %d\n", data);
+    usleep(data);
+  }
+}
+
 int main()
 {
   int pid;
+  int input_pids[MAX_PID];
+  int output_pids[MAX_PID];
   int status;
   int loop1, loop2;
   int shmid;
-  int *x;
+  /* int *x; */
+  _queue *x;
   int semid;
   struct sembuf sops;
-  
-  shmid = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
+  int pno;
+  time_t t;
+  srand((unsigned int)time(&t));
+
+  shmid = shmget(IPC_PRIVATE, sizeof(_queue), IPC_CREAT | 0666);
   if (shmid == -1) {
     perror("shmget");
     exit(1);
   }
-  x = (int *)shmat(shmid, NULL, 0);
-  if (x == (int *)-1) {
+  x = (_queue *)shmat(shmid, NULL, 0);
+  if (x == (_queue *)-1) {
     perror("shmat");
     exit(1);
   }
-  *x = 0;
+  // キューの初期化
+  x->size = 0;
+  x->wptr = 0;
+  x->rptr = 0;
 
   semid = semget(IPC_PRIVATE, 1, IPC_CREAT | 0666);
   if (semid == -1) {
@@ -62,27 +127,37 @@ int main()
     perror("semctl at initializing value of semaphore"); 
   }
 
-  pid = fork();
-  switch (pid) {
-  case    0:
-    printf("I am child process\n");
-    break;
-  case    -1:
-    printf("Fork error\n");
-    exit(1);
-  default:
-    printf("Process id of child process is %d\n",pid);
+  // 入力用プロセス生成
+  for ( pno = 0; pno < 1; pno++ ) {
+    input_pids[pno] = fork();
+    if ( input_pids[pno] == -1 ) {
+      break;
+    } else if (input_pids[pno] == 0) {
+      // 入力プロセスの処理
+      write_data(10, semid, x);
+      /* receiver(msgid, pno); */
+      return pno;
+    } else {
+      printf("Process id of input child process is %d\n",input_pids[pno]);
+    }      
   }
-  printf("%s: allocated address is %p\n", (pid) ? "Parent" : "Child ",x);
-  for (loop1 = 0; loop1 < TOTAL; loop1 += STEP) {
-    for (loop2 = 0; loop2 < STEP; loop2++) {
-      P(semid);
-      (*x)++;
-      V(semid);
-    }
-    printf("%s (%8d): x = %d\n", (pid) ? "Parent" : "Child ", loop1+loop2, *x);
+
+  // 入力用プロセス生成
+  for ( pno = 0; pno < 1; pno++ ) {
+    output_pids[pno] = fork();
+    if ( output_pids[pno] == -1 ) {
+      break;
+    } else if ( output_pids[pno] == 0 ) {
+      // 出力プロセスの処理
+      read_data(10, semid, x);
+      /* receiver(msgid, pno); */
+      return pno;
+    } else {
+      printf("Process id of output child process is %d\n",output_pids[pno]);
+    }      
   }
-  if (pid != 0) {
+
+  if (pno) {
     if (wait(&status) == -1) {
       perror("Wait error\n");
     } else {
